6d4cbc174fc9d5b3f9a440e301c540c77c8ef82b
duaboola <ismailshibiliya@gmail.com>
2023-12-18 23:22:10 +0300

    Initial Commit

---
+++ backend/passport.js
@@ -0,0 +1,61 @@
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const GithubStrategy = require("passport-github2").Strategy;
+const FacebookStrategy = require("passport-facebook").Strategy;
+const passport = require("passport");
+
+const GOOGLE_CLIENT_ID =
+  "505472082154-07tv11tn363atjmpirc6jik2sg6nvpqn.apps.googleusercontent.com";
+const GOOGLE_CLIENT_SECRET = "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";
+
+GITHUB_CLIENT_ID = "070f78f31617b8ac2179";
+GITHUB_CLIENT_SECRET = "e5150d5b634cc273d57ecb3bc35168ccaa08adb3";
+
+FACEBOOK_APP_ID = "facebook id";
+FACEBOOK_APP_SECRET = "facebook secret";
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: GOOGLE_CLIENT_ID,
+      clientSecret: GOOGLE_CLIENT_SECRET,
+      callbackURL: "/auth/google/callback",
+    },
+    function (accessToken, refreshToken, profile, done) {
+      done(null, profile);
+    }
+  )
+);
+
+passport.use(
+  new GithubStrategy(
+    {
+      clientID: GITHUB_CLIENT_ID,
+      clientSecret: GITHUB_CLIENT_SECRET,
+      callbackURL: "/auth/github/callback",
+    },
+    function (accessToken, refreshToken, profile, done) {
+      done(null, profile);
+    }
+  )
+);
+
+passport.use(
+  new FacebookStrategy(
+    {
+      clientID: FACEBOOK_APP_ID,
+      clientSecret: FACEBOOK_APP_SECRET,
+      callbackURL: "/auth/facebook/callback",
+    },
+    function (accessToken, refreshToken, profile, done) {
+      done(null, profile);
+    }
+  )
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+  done(null, user);
+});
---
+++ client/src/data.js
@@ -0,0 +1,26 @@
+export const posts = [
+    {
+      id: 1,
+      title: "OneID",
+      img: "https://beyonconnect.com/wp-content/uploads/2023/10/OneID-01-1.jpg",
+      desc: "Delivering the future of trusted digital identity",
+      longDesc:
+"Online identity verification typically involves confirming that the person providing information online is the same as the person they claim to be. With more services moving online, it’s important for online identity enrollment and verification to be as accessible and secure as possible.OneID by Beyon Connect is a digital identity and eKYC platform that enables individuals to enroll a variety of verified legal identity documents into a secure digital ID wallet, to use as a highly secure enrollment and authentication application across public and private sectors’ enhanced digital services.OneID is a universal and reusable Digital ID that gives people a faster, simpler and more secure way to prove their identity using their smartphone to capture and verify their facial biometrics. Individuals can enroll in minutes and complete online identity checks in seconds, speeding up and simplifying the way they prove who they are when using digital services. OneID makes identity verification available to more people, as they do not need to use physical documents every time they need to prove their identity.OneID is available as a mobile application for iOS, Android and Huawei and allows the onboarding of individuals through a simple and secure e-KYC process, supporting passport and national ID cards with facial recognition, liveness check and any official checks required by the trusting online service."    },
+    {
+      id: 2,
+      title: "OneBox",
+      img: "https://beyonconnect.com/wp-content/uploads/2023/10/oneBox-01-1.jpg",
+      desc: "The future of secure digital communication and value added e-services, all in one app",
+      longDesc:
+    "OneBox is an online digital mailbox and communication platform that enables secure, legally binding, and efficient communication between public sector entities, citizens, and businesses. OneBox solves many of the challenges of modern-day communication by creating a highly secure, spam free focused digital mailbox where you can receive and store important communication and documents from both trusted private and public sector organizations.Receiving messages from OneBox is free for all individuals and provides guaranteed delivery of notice for official letters, account statements and much more. Integrated value added services within the platform such as payments, signing and e-services, make this a ‘super-app’ for individuals and organizations."   
+    },
+    {
+      id: 3,
+      title: "OneSign",
+      img: "https://beyonconnect.com/wp-content/uploads/2023/10/OneSign-01.jpg",
+      desc: "Delivering digital signatures,simply and securely",
+      longDesc:
+    "OneSign is a qualified digital signing platform enabling a wide variety of documents to be legally signed online, around the clock, from any location. OneSign is embedded within OneID, the secure digital ID and authentication app."    
+    },
+  ];
+  
